d3f1a563205f8c52ee22eed50782cbba602efd79
Adam Czepiel <adam.czepiel@outlook.de>
2021-04-26 08:38:07 +0200

    Deutsche Bahn result

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const port = process.env.PORT || 3000;
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function(req, res) {
-  res.send('hello world');
+  res.send('hello Deutsche bahn Welt');
 });
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
-  })
+})
   
